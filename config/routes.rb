3a6c705cb25dc4b0ccf2accc92b361ec921903b7
Kyle Marsh <kmarsh6@stumail.jccc.edu>
2016-02-27 13:14:12 -0600

    form-based workflow feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-Blogger::Application.routes.draw do
-  resources :articles
-end
+  Blogger::Application.routes.draw do
+    resources :articles
+  end
 
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root to: 'articles#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
